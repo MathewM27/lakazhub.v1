31907f5648b1db23d20b76515056c7f9dfceeacb
Mathews Mwangi <mathewsmwangi6927@gmail.com>
2025-04-19 23:06:08 +0400

    Refactor imports to use dynamic loading for improved performance

--- app/(dashboard)/(landing)/page.tsx
+++ app/(dashboard)/(landing)/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { HeroSection } from "./components/sections/hero-section";
-import { WhyUsSection } from "./components/sections/why-us";
-import { SignupSection } from "./components/sections/signup-section";
 import { PageWrapper } from "./components/layout/PageWrapper";
-import { Footer } from "./components/navigation/footer";
-import { PropertySlider } from "./components/sections/property-slider";
+import dynamic from 'next/dynamic';
 
+const WhyUsSection = dynamic(() => import('./components/sections/why-us').then(mod => mod.WhyUsSection), { ssr: false });
+const PropertySlider = dynamic(() => import('./components/sections/property-slider').then(mod => mod.PropertySlider), { ssr: false });
+const SignupSection = dynamic(() => import('./components/sections/signup-section').then(mod => mod.SignupSection), { ssr: false });
+const Footer = dynamic(() => import('./components/navigation/footer').then(mod => mod.Footer), { ssr: false });
 
 export default function Home() {
   return (
